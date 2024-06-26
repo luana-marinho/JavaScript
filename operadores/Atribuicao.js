e53c9fd8cca09a2a461ecfe6cd32b0ec7096617c
Luana <luana.gabriela96@hotmail.com>
2024-06-26 15:39:42 -0300

    Lista de exercicios 01

---
+++ operadores/Atribuicao.js
@@ -0,0 +1,20 @@
+const leia = require('readline-sync')
+
+let quantidade;
+let altura;
+
+//quantidade = leia.questionInt("Digite um valor do tipo inteiro: ")
+//console.log("O valor inteiro digitado foi: " + quantidade)
+
+// altura = leia.questionFloat("Digite um valor do tipo Float: " , {limitMessage: 'Digite um número float'});
+// console.log("O valor Float digitado foi: " + altura);
+
+//Operadores
+
+let x = 6;
+let y = 2;
+let z = 1;
+let resul;
+
+resul = x - z;
+console.log("\nAtribuição com soma (x = x - z): " + resul);
